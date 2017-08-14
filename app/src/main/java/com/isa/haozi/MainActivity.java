40f0d7d0a72f25592188fb5c02689bfacec879a3
imononoke <3083545313@qq.com>
2017-08-14 18:49:18 +0800

    0814: 完善项目配置；增加悬浮按钮。

--- app/src/main/java/com/isa/haozi/MainActivity.java
+++ app/src/main/java/com/isa/haozi/MainActivity.java
@@ -1,9 +1,14 @@
 package com.isa.haozi;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.SimpleAdapter;
+
+import com.dianrong.android.dialog.CommonDialogBuilder;
+import com.dianrong.android.dialog.ICommonDialog;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,6 +18,8 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity {
 
     private GridView gridView;
+    private ImageButton ibAdd;
+
     private List<Map<String, Object>> list;
 
     @Override
@@ -24,10 +31,16 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
         gridView = (GridView) findViewById(R.id.gridView);
+        ibAdd = (ImageButton) findViewById(R.id.ibAdd);
         initData();
 
         SimpleAdapter adapter = new SimpleAdapter(this, list, R.layout.layout_item, new String[] {"image", "name"}, new int[] {R.id.image, R.id.tvName});
         gridView.setAdapter(adapter);
+
+        ibAdd.setOnClickListener(v -> {
+            // show set folder dialog
+            createFolderDialog();
+        });
     }
 
     private void initData() {
@@ -41,5 +54,21 @@ public class MainActivity extends AppCompatActivity {
         map.put("image", R.drawable.cat_pink);
         map.put("name", getString(R.string.specialMoment));
         list.add(map);
+    }
+
+    private void createFolderDialog() {
+        CommonDialogBuilder builder = new CommonDialogBuilder(this);
+        builder.setTitle("Create folder");
+        builder.setCanceledOnTouchOutside(true);
+        builder.setCustomView(getLayoutInflater().inflate(R.layout.layout_create_folder, null));
+        builder.setButton(DialogInterface.BUTTON_POSITIVE, "OK");
+        builder.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel");
+
+        builder.setOnClickListener((dialogInterface, i) -> {
+            dialogInterface.dismiss();
+        });
+
+        ICommonDialog iCommonDialog = builder.create();
+        iCommonDialog.show();
     }
 }
